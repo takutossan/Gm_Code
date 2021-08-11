7bb9777a6d22a0752182b7a28a7856aa923f2556
takutossan <gikushaku.4989@gmail.com>
2021-08-11 16:19:03 +0900

    first commit

---
+++ app/javascript/code.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+function sound()
+{
+	document.getElementById('sound').play() ;
+}
+});
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: 'codes#index'
+  resources :codes, only: :index
+ end
