ad54f8bb0e315cdddf142227b14a604b1d0728fc
takutossan <gikushaku.4989@gmail.com>
2021-08-11 16:44:19 +0900

    コントローラ ルーティング設定

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -1,4 +1,23 @@
 class CodesController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
   def index
+  end
+
+  def new
+    @code = Code.new
+    end
+  
+    def create
+      @code = Code.new(code_params)
+      if @code.save
+        redirect_to root_path
+      else
+        render :new
+      end
+    end
+
+    private
+  def code_params
+    params.require(:code).permit(:image, :message, :height, :age).merge(user_id: current_user.id)
   end
 end
