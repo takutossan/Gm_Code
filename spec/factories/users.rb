dc5922450b7ee9e33da61c4dd756527977f7d8d8
takutossan <gikushaku.4989@gmail.com>
2021-08-12 15:51:31 +0900

    テストコードと新規投稿ページのビュー作成

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :user do
+    transient do
+      person { Gimei.name }
+    end
+    name {Faker::Name.initials(number: 6)}
+    email                 { Faker::Internet.free_email }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+  end
+end
