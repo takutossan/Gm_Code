7c5f611cb88ed08f6caced61ea3e2c665df7822a
takutossan <gikushaku.4989@gmail.com>
2021-08-16 11:06:48 +0900

    編集ページルーティング設定まで

--- app/javascript/code.js
+++ app/javascript/code.js
@@ -1,22 +1,2 @@
 window.addEventListener('load', () => {
-	var moreNum = 5;
-
-	/* 表示するリストの数以降のリストを隠しておきます。 */
-	$('.code-lists:nth-child(n + ' + (moreNum + 1) + ')').addClass('is-hidden');
-	
-	/* 全てのリストを表示したら「もっとみる」ボタンをフェードアウトします。 */
-	$('.list-btn').on('click', function() {
-		$('.code-lists.is-hidden').slice(0, moreNum).removeClass('is-hidden');
-		if ($('.code-lists.is-hidden').length == 0) {
-			$('.list-btn').fadeOut();
-		} 
-	});
-	
-	/* リストの数が、表示するリストの数以下だった場合、「もっとみる」ボタンを非表示にします。 */
-	$(function() {
-		var list = $(".list li").length;  
-			if (list < moreNum) {
-				$('.list-btn').addClass('is-btn-hidden');
-		}
-	});
 });
